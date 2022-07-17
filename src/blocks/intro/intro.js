b9ee17459f0d43b97e1a8baf4b235810e3940a88
Pavel <pavellapunov@inbox.ru>
2022-07-17 19:57:56 +0300

    add second page, start adaptive

--- src/blocks/intro/intro.js
+++ src/blocks/intro/intro.js
@@ -1,6 +1,5 @@
 (function () {
-    document.querySelector( '.intro__range' ).addEventListener( 'input', function( ) {
-        this.style='background:linear-gradient(to right, #1146A6 0%, #1146A6 '+this.value +'%, #fff ' + this.value + '%, white 100%)'
-        //this.style( 'background', 'linear-gradient(to right, green 0%, green '+this.value +'%, #fff ' + this.value + '%, white 100%)' );
+    document.querySelector( '.intro__range-track' ).addEventListener( 'input', function( ) {
+        this.style='background:linear-gradient(to right, #1146A6 0%, #1146A6 '+this.value +'%, #fff ' + this.value + '%, white 100%)';
       } );
 })();
