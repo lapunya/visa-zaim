4b639af84215313eb6fbfa4985df9b7512324356
Pavel <pavellapunov@inbox.ru>
2022-07-17 23:38:45 +0300

    finished adaptive

--- src/blocks/intro/intro.js
+++ src/blocks/intro/intro.js
@@ -1,5 +1,8 @@
 (function () {
-    document.querySelector( '.intro__range-track' ).addEventListener( 'input', function( ) {
-        this.style='background:linear-gradient(to right, #1146A6 0%, #1146A6 '+this.value +'%, #fff ' + this.value + '%, white 100%)';
-      } );
+  let intro = document.querySelector('.intro');
+  if (intro) {
+    intro.querySelector('.intro__range-track').addEventListener('input', function () {
+      this.style = 'background:linear-gradient(to right, #1146A6 0%, #1146A6 ' + this.value + '%, #fff ' + this.value + '%, white 100%)';
+    });
+  }
 })();
