7653d2c7375c34e53c5a3b646c01a6d481f58cf5
Pavel <pavellapunov@inbox.ru>
2022-07-18 23:33:03 +0300

    final commit 1

--- src/blocks/intro/intro.js
+++ src/blocks/intro/intro.js
@@ -1,8 +1,30 @@
 (function () {
-  let intro = document.querySelector('.intro');
-  if (intro) {
-    intro.querySelector('.intro__range-track').addEventListener('input', function () {
-      this.style = 'background:linear-gradient(to right, #1146A6 0%, #1146A6 ' + this.value + '%, #fff ' + this.value + '%, white 100%)';
+  let form = document.querySelector('#intro-form');
+  if (form) {
+    let range = form.querySelector('.intro__range-track');
+    let output = form.querySelector('.intro__input--sum');
+    function getRangeFillValue(value) {
+      return 'background:linear-gradient(to right, #1146A6 0%, #1146A6 ' + value / 1000 + '%, #fff ' + value / 1000 + '%, white 100%)';
+    }
+
+    output.value = range.value;
+    range.value = output.value;
+    range.style = getRangeFillValue(range.value);
+
+    range.addEventListener('input', function () {
+      this.style = getRangeFillValue(this.value);
+      output.value = this.value;
     });
+
+    output.addEventListener('input', function() {
+      if (this.value < +this.min) {
+        this.value = this.min;
+      }
+      if (this.value > +this.max) {
+        this.value = this.max;
+      }
+      range.value = this.value;
+      range.style =  getRangeFillValue(this.value);
+    })
   }
 })();
